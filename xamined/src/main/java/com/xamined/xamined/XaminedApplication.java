965e9792d18545c6fd3541b7dec17bfd213b822a
MahgoubHusien <mahgoubhhusien@gmail.com>
2024-09-10 22:47:09 -0500

    Setup the frontend and Backend

---
+++ xamined/src/main/java/com/xamined/xamined/XaminedApplication.java
@@ -0,0 +1,13 @@
+package com.xamined.xamined;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XaminedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XaminedApplication.class, args);
+	}
+
+}
